445b90a996aef1cb4b44225835682e89d3e00b1c
abeerlit <abeer.litcheck@gmail.com>
2024-10-17 19:54:53 +0500

    ...

---
+++ src/app/api/admin/[id]/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../../../config/prisma';
+
+export async function GET({ params }: any) {
+  const { id } = params;
+  if (!id || isNaN(id)) {
+    return NextResponse.json({ error: 'Invalid user ID.' }, { status: 400 });
+  }
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(id, 10) },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found.' }, { status: 404 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: 'Error fetching user by ID.' },
+      { status: 500 }
+    );
+  }
+}
